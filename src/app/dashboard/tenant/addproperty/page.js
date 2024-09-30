dfe20ae2f45da956a318e04612826247afbd99f7
sreesachithb@gmail.com <sreesachithb@gmail.com>
2024-09-30 10:24:16 +0530

    added tenant dashboard page similar to the owner with changes for plaid

---
+++ src/app/dashboard/tenant/addproperty/page.js
@@ -0,0 +1,10 @@
+import '../dashboard.css';
+import PropertyForm from "@/components/propertyForm/PropertyForm";
+
+export default function AddProperty() {
+  return (
+    <div className="container">
+    <PropertyForm/>
+    </div>
+  );
+}
